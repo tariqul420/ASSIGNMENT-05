7a159f5c562ff3c279f0f75c0e988c016a78e2ab
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-09-23 05:57:56 +0600

    noakhali donate done

--- js/common.js
+++ js/common.js
@@ -1,4 +1,19 @@
 function inputValue(id) {
-    const value = document.getElementById(id).value;
-    return parseFloat(value.toFixed(2));
-};
+  const values = document.getElementById(id).value;
+  const number = parseFloat(values);
+  return number;
+}
+
+function common(inputId , donateId) {
+  const inputNum = inputValue(inputId);
+
+  const mainAmount = document.getElementById("main-amount").innerText;
+  let mainNum = parseFloat(mainAmount);
+  let currentMain = mainNum - inputNum;
+  document.getElementById("main-amount").innerText = currentMain.toFixed(2);
+
+  const donateAmount = document.getElementById(donateId).innerText;
+  let donateNum = parseFloat(donateAmount);
+  let currentDonate = donateNum + inputNum;
+  document.getElementById("donate-amount").innerText = currentDonate.toFixed(2);
+}
--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
 const donateBtn = document.getElementById("donate-btn");
 const historyBtn = document.getElementById("history-btn");
+const donateAllEl = document.getElementById("donate-container");
+const historyAllEl = document.getElementById("history-container");
 
-donateBtn.addEventListener('click',function(){
-    donateBtn.classList.add('bg-primary', 'p-[12px_32px_12px_32px]', 'text-dark-01')
-    console.log("donate");
+donateBtn.addEventListener("click", function () {
+  donateBtn.classList.add("bg-primary", "p-[12px_32px_12px_32px]", "text-dark-01");
 
-    donateBtn.classList.remove('border-2', 'border-solid', 'border-dark-03', 'p-[10px_30px_10px_30px]', 'text-dark-02');
+  donateBtn.classList.remove("border-2", "border-solid", "border-dark-03", "p-[10px_30px_10px_30px]", "text-dark-02");
 
-    historyBtn.classList.add('border-2', 'border-solid', 'border-dark-03', 'p-[10px_30px_10px_30px]', 'text-dark-02');
+  historyBtn.classList.add("border-2", "border-solid", "border-dark-03", "p-[10px_30px_10px_30px]", "text-dark-02");
 
-    historyBtn.classList.remove('bg-primary', 'p-[12px_32px_12px_32px]', 'text-dark-01');
+  historyBtn.classList.remove("bg-primary", "p-[12px_32px_12px_32px]", "text-dark-01");
+
+  donateAllEl.classList.remove("hidden");
+
+  historyAllEl.classList.add("hidden");
 });
 
-historyBtn.addEventListener('click',function(){
-    donateBtn.classList.remove('bg-primary', 'p-[12px_32px_12px_32px]', 'text-dark-01')
-    console.log("donate");
+historyBtn.addEventListener("click", function () {
+  donateBtn.classList.remove("bg-primary", "p-[12px_32px_12px_32px]", "text-dark-01");
+  console.log("donate");
 
-    donateBtn.classList.add('border-2', 'border-solid', 'border-dark-03', 'p-[10px_30px_10px_30px]', 'text-dark-02');
+  donateBtn.classList.add("border-2", "border-solid", "border-dark-03", "p-[10px_30px_10px_30px]", "text-dark-02");
 
-    historyBtn.classList.remove('border-2', 'border-solid', 'border-dark-03', 'p-[10px_30px_10px_30px]', 'text-dark-02');
+  historyBtn.classList.remove("border-2", "border-solid", "border-dark-03", "p-[10px_30px_10px_30px]", "text-dark-02");
 
-    historyBtn.classList.add('bg-primary', 'p-[12px_32px_12px_32px]', 'text-dark-01');
+  historyBtn.classList.add("bg-primary", "p-[12px_32px_12px_32px]", "text-dark-01");
+
+  donateAllEl.classList.add("hidden");
+
+  historyAllEl.classList.remove("hidden");
+});
+
+// done donate fot noakhali
+document.getElementById("donate-btn-noakhali").addEventListener("click", function () {
+  inputValue("donate-input-noakhali");
+
+  common("donate-input-noakhali", 'donate-amount')
 });
